27ba7bec7a87e92ca21382d6e8bc9fd662367135
viethieu <truongviethieu@gmail.com>
2020-11-11 14:24:29 +0700

    t

---
+++ ShopSWE/ShopSWE/Models/ViewModels/banner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopSWE.Models.Enity;
+
+namespace ShopSWE.Models.ViewModels
+{
+    public class banner
+    {
+        public List<Banner> banners { get; set; }
+    }
+}
